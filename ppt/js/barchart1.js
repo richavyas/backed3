dd5fa197bab8ded04ca6d17b1ca5dbba10eaddcd
Abhishek Gayakwad <a.gayakwad@gmail.com>
2013-06-05 01:31:39 +0530

    final PPT format

---
+++ ppt/js/barchart1.js
@@ -0,0 +1,88 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>404 - Sadique Ali</title>
+    <meta name="author" content="Sadique Ali">
+
+    
+    <meta name="description" content="Well, I don&rsquo;t remember creating that page.
+May be you typed a wrong URL. You should look at the Archives to see blog posts.
+">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link href="/atom.xml" rel="alternate" title="Sadique Ali" type="application/atom+xml">
+    <link rel="canonical" href="">
+    <link href="/favicon.png" rel="shortcut icon">
+    <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <link href="http://fonts.googleapis.com/css?family=Alegreya"
+          rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Sansita+One"
+          rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Code+Pro"
+          rel="stylesheet" type="text/css">
+</head>
+
+
+<body>
+    <div class="container">
+        <div class="main-nav">
+            <header id="header" class="inner"><nav id="main-nav"><ul class="main">
+    <li><a href="/">Home</a></li>
+    <li><a href="/blog">Blog</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/talks">Talks</a></li>
+    <li><a class ="rss" href="/atom.xml">Feed</a></li>
+    <li><a href="https://github.com/sdqali">GitHub</a></li>
+    <li><a href="https://twitter.com/sdqali">Twitter</a></li>
+</ul>
+</nav>
+</header>
+        </div>
+        <div class="main">
+          <div id="content" class="inner"><article class="page">
+    
+    <h1 class="title">404</h1>
+    
+    <div class="entry-content"><p>Well, I don&rsquo;t remember creating that page.
+May be you typed a wrong URL.</p>
+
+<p>You should look at the <a href="/blog/archives">Archives</a> to see blog posts.</p>
+</div>
+</article>
+
+</div>
+          <footer id="footer" class="inner">Copyright &copy; 2008 - 2013
+
+  Sadique Ali
+.
+
+
+<section>
+  <script type="text/javascript">
+    var sc_project=7579692;
+    var sc_invisible=1;
+    var sc_security="419588d3";
+  </script>
+  <script type="text/javascript"
+          src="http://www.statcounter.com/counter/counter_xhtml.js"></script>
+  <noscript><div class="statcounter"><a title="tumblr
+                                               analytics" href="http://statcounter.com/tumblr/"
+                                        class="statcounter"><img class="statcounter"
+                                                                 src="http://c.statcounter.com//0//0/"
+                                                                 alt="tumblr analytics" /></a></div></noscript>
+</section>
+
+</footer>
+          
+
+
+
+        </div>
+    </div>
+</body>
+</html>
